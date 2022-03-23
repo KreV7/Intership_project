6930b24eef676eae72f48af544bd22064cb8b47c
KreV - Home <kruchkovei@gmail.com>
2022-03-23 11:31:10 +0300

    Create models

---
+++ mainapp/models/__init__.py
@@ -0,0 +1,5 @@
+from .customers import AdvUser
+from .car import Car
+from .suppliers import Supplier, SuppliersGarage
+from .showrooms import Showroom, ShowroomsGarage, ShowroomCustomerHistory, SupplierSalesHistory
+
---
+++ mainapp/models/suppliers.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from .car import Car
+
+
+class Supplier(models.Model):
+    title = models.CharField(max_length=128)
+    year_foundation = models.DateField()
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def number_of_buyers(self):
+        pass
+
+
+class SuppliersGarage(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=25, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
